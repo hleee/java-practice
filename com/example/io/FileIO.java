ab7ffdc5999c1f6317b479165b6b5a7716e7284f
hleee <hynsngl@gmail.com>
2019-12-25 23:31:03 +0900

    Final refactoring

--- com/example/io/FileIO.java
+++ com/example/io/FileIO.java
@@ -5,29 +5,27 @@ import java.io.*;
 public class FileIO {
 
     public static void main(String[] args) throws IOException {
-        String fileName = "C:\\Users\\hlee\\Documents\\GitHub\\java-practice\\com\\example\\io\\input.txt";
+        String inputFileName = "C:\\Users\\hlee\\Documents\\GitHub\\java-practice\\com\\example\\io\\input.txt";
         String outputFileName = "C:\\Users\\hlee\\Documents\\GitHub\\java-practice\\com\\example\\io\\output.txt";
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader = new BufferedReader(new FileReader(inputFileName));
             BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
 
             String line = reader.readLine();
             int lineNumber = 1;
             while (line != null) {
-                String inputLine = String.format("Name on Line #%d: %s", lineNumber, line + "\n");
+                String inputLine = String.format("Name on Line #%d: %s\n", lineNumber, line);
                 System.out.print(inputLine);
+                writer.write(inputLine);
+
                 line = reader.readLine();
                 lineNumber++;
-
-                writer.write(inputLine);
             }
             reader.close();
             writer.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
