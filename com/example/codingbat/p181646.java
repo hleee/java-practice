1f26a73aa5ec30d7801c578b165bf15268f7ed06
hleee <hynsngl@gmail.com>
2019-12-25 18:11:53 +0900

    Simplify boolean expressions

--- com/example/codingbat/p181646.java
+++ com/example/codingbat/p181646.java
@@ -3,19 +3,13 @@ package com.example.codingbat;
 public class p181646 {
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
-        if (aSmile == true && bSmile == true) {
+        if (aSmile && bSmile) {
             return true;
-        } else if (aSmile == false && bSmile == false) {
-            return true;
-        } else {
-            return false;
-        }
+        } else return !aSmile && !bSmile;
     }
 
     public static void main(String[] args) {
         p181646 mt = new p181646();
         mt.monkeyTrouble(true, false);
     }
-
-
 }
