b97f1503125b588180c116c202841062fd69f107
hleee <hynsngl@gmail.com>
2019-12-25 23:24:34 +0900

    Output to text file

--- com/example/io/FileIO.java
+++ com/example/io/FileIO.java
@@ -1,27 +1,33 @@
 package com.example.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileIO {
 
     public static void main(String[] args) throws IOException {
         String fileName = "C:\\Users\\hlee\\Documents\\GitHub\\java-practice\\com\\example\\io\\input.txt";
+        String outputFileName = "C:\\Users\\hlee\\Documents\\GitHub\\java-practice\\com\\example\\io\\output.txt";
+
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
 
             String line = reader.readLine();
             int lineNumber = 1;
             while (line != null) {
-                String inputLine = String.format("Name on Line #%d: %s", lineNumber, line);
-                System.out.println(inputLine);
+                String inputLine = String.format("Name on Line #%d: %s", lineNumber, line + "\n");
+                System.out.print(inputLine);
                 line = reader.readLine();
                 lineNumber++;
+
+                writer.write(inputLine);
             }
             reader.close();
+            writer.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
