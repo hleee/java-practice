ef5e249545fe16c2fa378a2f5f1271b49c243a66
hleee <hynsngl@gmail.com>
2019-12-25 17:42:56 +0900

    Restructure java programs

---
+++ com/example/codingbat/p125339.java
@@ -0,0 +1,26 @@
+package com.example.codingbat;
+/* Problem
+Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57.
+Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+lastDigit(7, 17) → true
+lastDigit(6, 17) → false
+lastDigit(3, 113) → true
+*/
+
+public class p125339 {
+public boolean lastDigit(int a, int b) {
+    return Math.abs(a)%10 == Math.abs(b)%10;
+}
+    public static void main(String[] args) {
+        p125339 p = new p125339();
+        p.lastDigit(3, 1);
+    }
+}
+
+/* Solution
+public boolean lastDigit(int a, int b) {
+  // True if the last digits are the same
+  return(a % 10 == b % 10);
+}
+*/
---
+++ com/example/codingbat/p182873.java
@@ -0,0 +1,28 @@
+package com.example.codingbat;
+/* Problem
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+*/
+
+public class p182873{
+public boolean makes10(int a, int b) {
+  if(a == 10 || b== 10 || a + b == 10) {
+    return true;
+  } else {
+    return false;
+  }
+}
+    public static void main(String[] args) {
+        p182873 p1 = new p182873();
+        p1.makes10(1, 2);
+    }
+}
+
+/* Solution
+public boolean makes10(int a, int b) {
+  return (a == 10 || b == 10 || a+b == 10);
+}
+*/
