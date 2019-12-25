1e75771da2834134441cc9e3a388b511b2ef4f1b
hleee <hynsngl@gmail.com>
2019-12-25 18:21:43 +0900

    Add problem statement

--- com/example/codingbat/p181646.java
+++ com/example/codingbat/p181646.java
@@ -1,5 +1,12 @@
 package com.example.codingbat;
 
+/*
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate
+if each is smiling.
+We are in trouble if they are both smiling or if neither of them is smiling.
+Return true if we are in trouble.
+ */
+
 public class p181646 {
 
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
